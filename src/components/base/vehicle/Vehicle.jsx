00d2c213e53f143155caec8b99554a35680a29d3
pedroluiznogueira <peluiznogueiram@hotmail.com>
2022-01-11 17:07:49 -0300

    admin vehicle icons layout

--- src/components/base/vehicle/Vehicle.jsx
+++ src/components/base/vehicle/Vehicle.jsx
@@ -2,17 +2,21 @@ import './Vehicle.css';
 import webmotors from '../../shared/assets/webmotors.jpg';
 import VehicleContext from '../../context/vehicle/VehicleContext';
 import { useContext, useState, useEffect } from 'react';
+import { FaTrash, FaPlus, FaEdit } from 'react-icons/fa';
 
 function Vehicle() {
-    const [catalog, setCatalog] = useState([]);    
+    const [catalog, setCatalog] = useState([]);
+    const [isAdmin, setIsAdmin] = useState('');
     const {vehicles, fetchVehicles} = useContext(VehicleContext);
 
+    
     useEffect(() => {
-        const isAdmin = window.sessionStorage.getItem('isAdmin');
-        
+        const admin = window.sessionStorage.getItem('isAdmin');
+        setIsAdmin(admin);
         setCatalog(vehicles);
     }, fetchVehicles);
     
+
     return (
         <div className="catalog">
             {catalog.map((vehicle) => (
@@ -26,8 +30,17 @@ function Vehicle() {
                             <h2 className="price">$2K</h2>
                         </div>
                         <div className="bottom-block">
-                            <div className="model" style={{color: 'white'}}>{vehicle.model}</div>
-                            <div className="brand" style={{color: 'white'}}>{vehicle.brand}</div>
+                            <div className="text-block">
+                                <div className="model" style={{color: 'white'}}>{vehicle.model}</div>
+                                <div className="brand" style={{color: 'white'}}>{vehicle.brand}</div>
+                            </div>
+                            {isAdmin === 'false' && (
+                                <div className="icon-block">
+                                    <FaPlus className="icon" />
+                                    <FaTrash className="icon" />
+                                    <FaEdit className="icon" />
+                                </div>
+                            )}
                         </div>
                     </div>
                 </div>

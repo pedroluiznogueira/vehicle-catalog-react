87f870a05c5e060efeef60bc206a3619392d30f1
pedroluiznogueira <peluiznogueiram@hotmail.com>
2022-01-11 01:07:14 -0300

    fetching vehicles using useEffect()

--- src/components/context/vehicle/VehicleContext.jsx
+++ src/components/context/vehicle/VehicleContext.jsx
@@ -3,8 +3,23 @@ import { createContext, useState, useEffect } from 'react';
 const VehicleContext = createContext();
 
 export const VehicleProvider = ( {children} ) => {
+    useEffect(() => {
+        fetchVehicles();
+    }, []);
 
-    
+    const fetchVehicles = async () => {
+        let token = window.sessionStorage.getItem('token');
+        const response = await fetch('http://localhost:8080/vehicles/find/all', {
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + token
+            },
+        });
+        const data = await response.json();
+        console.log(data);
+        return data;
+    }
 
     return(
         <VehicleContext.Provider value={{}}>

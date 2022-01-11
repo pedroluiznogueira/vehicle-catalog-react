95c55ebd0df491aa2d54b6aa07e069bfd536e3e2
pedroluiznogueira <peluiznogueiram@hotmail.com>
2022-01-10 23:16:01 -0300

    register form layout

--- src/components/auth/register/Register.jsx
+++ src/components/auth/register/Register.jsx
@@ -1,9 +1,72 @@
+import { NavLink } from "react-router-dom";
+import './Register.css';
+
 function Register() {
-    return (
-        <h1>
-            Register
-        </h1>
-    )
+    const handleSubmit = (e) => {
+    }
+
+    const handleNameChange = (e) => {
+    }
+
+    const handleEmailChange = (e) => {
+    }
+
+    const handlePasswordChange = (e) => {
+    }
+
+    return(
+        <div className="container">
+            <form className="register-form" onSubmit={handleSubmit}>
+                <h1>Cadastro</h1>
+                <div className="form-group">
+                    <label htmlFor="exampleInputNome">Nome</label>
+                    <input
+                        onChange={handleNameChange}
+                        type="text" 
+                        className="form-control" 
+                        id="exampleInputNome" 
+                        aria-describedby="emailHelp" 
+                        placeholder="Name" 
+                        name="name"
+    
+                    />
+                </div>
+                <div className="form-group">
+                <label htmlFor="exampleInputEmail1">E-mail</label>
+                <input
+                    onChange={handleEmailChange}
+                    type="email" 
+                    className="form-control" 
+                    id="exampleInputEmail1" 
+                    aria-describedby="emailHelp" 
+                    placeholder="Email" 
+                    name="email"
+
+                />
+                </div>
+                <div className="form-group">
+                <label htmlFor="exampleInputPassword1">Senha</label>
+                <input
+                    onChange={handlePasswordChange}
+                    type="password" 
+                    className="form-control" 
+                    id="exampleInputPassword1" 
+                    placeholder="Password" 
+                    name="password"
+   
+                />
+                </div>
+                <div className="form-group-question">
+                    <small>
+                        <NavLink className="question-text" to='/login' style={{ textDecoration: 'none' }}>
+                            Já tem uma conta ?
+                        </NavLink>
+                    </small>
+                </div>
+                <button className="btn" type="submit">Criar conta</button>
+            </form>
+        </div>
+    );
 }
 
 export default Register;

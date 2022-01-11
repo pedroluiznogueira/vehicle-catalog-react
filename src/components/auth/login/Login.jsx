030050947443e4fccd70f51f3c00ea914765cf2e
pedroluiznogueira <peluiznogueiram@hotmail.com>
2022-01-10 23:10:42 -0300

    login form layout

--- src/components/auth/login/Login.jsx
+++ src/components/auth/login/Login.jsx
@@ -1,16 +1,60 @@
-import { useNavigate } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
+import './Login.css';
 
 function Login() {
     const navigate = useNavigate();
     
-    const handleClick = () => {
+    const handleSubmit = (e) => {
         window.sessionStorage.setItem('mockedToken', 'mockedToken');
         navigate('/app');
     }
 
-    return (
-            <button onClick={handleClick}>Login</button>
-    )
+    const handleEmailChange = (e) => {
+    }
+
+    const handlePasswordChange = (e) => {
+    }
+
+    return(
+        <div className="container">
+            <form className="form" onSubmit={handleSubmit}>
+                <h1>Login</h1>
+                <div className="form-group">
+                    <label htmlFor="exampleInputEmail1">E-mail</label>
+                    <input
+                        onChange={handleEmailChange}
+                        type="email" 
+                        className="form-control" 
+                        id="exampleInputEmail1" 
+                        aria-describedby="emailHelp" 
+                        placeholder="Enter email" 
+                        name="email"
+
+                    />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="exampleInputPassword1">Senha</label>
+                    <input
+                        onChange={handlePasswordChange}
+                        type="password" 
+                        className="form-control" 
+                        id="exampleInputPassword1" 
+                        placeholder="Password" 
+                        name="password" 
+                    
+                    />
+                </div>
+                <div className="form-group-question">
+                    <small>
+                        <NavLink className="question-text" to='/register' style={{ textDecoration: 'none' }}>
+                            Ainda não tem uma conta ?
+                        </NavLink>
+                    </small>
+                </div>
+                <button className="btn" type="submit">Entrar</button>
+            </form>
+        </div>
+    );
 }
 
 export default Login;

aaf5862833e40a18fe04e1a70b7bdaec62cf44bb
pedroluiznogueira <peluiznogueiram@hotmail.com>
2022-01-12 21:21:51 -0300

    fix bug 'Invalid Host header'

--- src/components/context/vehicle/VehicleContext.jsx
+++ src/components/context/vehicle/VehicleContext.jsx
@@ -10,13 +10,14 @@ export const VehicleProvider = ( {children} ) => {
 
     const token = window.sessionStorage.getItem('token');
     const userId = window.sessionStorage.getItem('logged');
+    const url = 'https://vehicle-catalog-api.herokuapp.com';
 
     useEffect(() => {
         fetchVehicles();
     }, []);
 
     const fetchVehicles = async () => {
-        const response = await fetch(`/vehicles/find/all`, {
+        const response = await fetch(`${url}/vehicles/find/all`, {
             method: 'GET',
             headers: { 
                 'Authorization': 'Bearer ' + token,
@@ -29,7 +30,7 @@ export const VehicleProvider = ( {children} ) => {
     }
 
     const uploadFile = async (formData) => {
-        const response = await axios.post(`/upload-file`, formData, {
+        const response = await axios.post(`${url}/upload-file`, formData, {
             headers: {'Authorization': 'Bearer ' + token}
         });
     }
@@ -39,7 +40,7 @@ export const VehicleProvider = ( {children} ) => {
     }
 
     const registerVehicle = async (vehicle) => { 
-        const response = await fetch(`/vehicles/register/${userId}`, {
+        const response = await fetch(`${url}/vehicles/register/${userId}`, {
             method: 'POST',
             headers: {
                 'Accept': 'application/json',
@@ -58,7 +59,7 @@ export const VehicleProvider = ( {children} ) => {
     }
 
     const updateVehicle = async (vehicle) => {
-        const response = await fetch(`/vehicles/update/${vehicleId}/user/${userId}`, {
+        const response = await fetch(`${url}/vehicles/update/${vehicleId}/user/${userId}`, {
             method: 'PUT',
             headers: {
                 'Accept': 'application/json',
@@ -75,7 +76,7 @@ export const VehicleProvider = ( {children} ) => {
     }
 
     const deleteVehicle = async (vehicleId) => {
-        const response = await fetch(`/vehicles/delete/${vehicleId}/user/${userId}`, {
+        const response = await fetch(`${url}/vehicles/delete/${vehicleId}/user/${userId}`, {
             method: 'DELETE',
             headers: {
                 'Accept': 'application/json',
